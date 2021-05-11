22c6a1d2b376434f522f3ad8f12613ee61c3d8f1
Itzel01 <itzel.areymi.rendon@gmail.com>
2021-05-11 12:17:24 -0400

    removed more -

--- 2Peers-Backend/database/init.sql
+++ 2Peers-Backend/database/init.sql
@@ -9,15 +9,15 @@ DROP TABLE IF EXISTS teacherMessages;
 CREATE TABLE subjects (
     id SERIAL PRIMARY KEY,
     name TEXT
-)
+);
 
 CREATE TABLE teachers (
     id SERIAL PRIMARY KEY,
     name TEXT, 
     email TEXT, 
-    profile-pic Text, 
+    profilePic Text, 
     subject int, 
-    encrypted_password TEXT,
+    encryptedPassword TEXT,
     archived BOOLEAN,
     FOREIGN KEY (subject) REFERENCES subjects(id)
 );
@@ -26,21 +26,21 @@ CREATE TABLE students (
     id SERIAL PRIMARY KEY,
     name TEXT, 
     email TEXT, 
-    profile-pic Text, 
-    encrypted_password TEXT,
+    profilePic Text, 
+    encryptedPassword TEXT,
     archived BOOLEAN
 );
 
 CREATE TABLE classes (
     id SERIAL PRIMARY KEY,
-    class-code TEXT
+    classCode TEXT
 );
 
 CREATE TABLE classMembers (
     id SERIAL PRIMARY KEY,
     student int,
-    self-rating int,
-    peer-rating int,
+    selfRating int,
+    peerRating int,
     FOREIGN KEY (student) REFERENCES students(id)
 );
 
@@ -49,7 +49,7 @@ CREATE TABLE studentMessages (
     student int,
     class int, 
     message TEXT,
-    message-rating int, 
+    messageRating int, 
     date now(),
     FOREIGN KEY (student) REFERENCES students(id),
     FOREIGN KEY (class) REFERENCES classes(id)
@@ -60,7 +60,7 @@ CREATE TABLE teacherMessages (
     teacher int,
     class int, 
     message TEXT,
-    message-rating int, 
+    messageRating int, 
     date now(),
     FOREIGN KEY (teacher) REFERENCES teachers(id),
     FOREIGN KEY (class) REFERENCES classes(id)
