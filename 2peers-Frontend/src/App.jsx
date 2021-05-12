a8e60469be6753779e1b62c8bda6fbd3363c551b
jason <jasonalvarezcoraiza@gmail.com>
2021-05-12 13:21:35 -0400

    made editing view for student

--- 2peers-Frontend/src/App.jsx
+++ 2peers-Frontend/src/App.jsx
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 // import Classroom from './Classroom/Classroom';
-import Student from './Student/Student';
+// import Student from './Student/Student';
+import EditStudent from './Student/EditStudent';
 // import Navbar from './Reusable/Navbar';
 
 function App() {
@@ -21,7 +22,7 @@ function App() {
         <p>{data || 'Loading...'}</p>
       </header>
       {/* <Classroom /> */}
-      <Student />
+      <EditStudent />
     </div>
   );
 }
--- 2peers-Frontend/src/Student/EditStudent.jsx
+++ 2peers-Frontend/src/Student/EditStudent.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function EditStudent() {
+  const [name, setName] = useState('');
+
+  function sendEditedStudent(e) {
+    e.preventDefault();
+  }
+
   return (
     <div className="editStudentPage">
-      <p>Editing Student</p>
+      <h1>Editing Student Page</h1>
+      <div className="student-info flex justify-center">
+        <form className="w-9/12" onSubmit={sendEditedStudent}>
+          <label
+            htmlFor="name"
+            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2 flex flex-wrap -mx-3 mb-6"
+          >
+            <p className="mb-5">Name</p>
+            <br />
+            <input
+              id="name"
+              value={name}
+              type="text"
+              onChange={({ target }) => { setName(target.value); }}
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-green-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+            />
+            <br />
+            <input
+              readOnly
+              type="submit"
+              value="Submit"
+              className="bg-green-400 hover:bg-green-500 p-1 px-2 rounded text-white my-1 focus:shadow-outline focus:outline-none text-base"
+            />
+          </label>
+        </form>
+      </div>
     </div>
   );
 }
