bb6f7d2a23a06bba799399a1bf6772ffdcac8edc
jason <jasonalvarezcoraiza@gmail.com>
2021-05-15 14:57:30 -0400

    slight change to app for front end testing

--- 2peers-Frontend/src/App.jsx
+++ 2peers-Frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Login from './data/Login';
 import Signup from './data/Signup';
 import Page404 from './data/Page404';
-// import Message from './Classroom/Message';
+import Classroom from './Classroom/Classroom';
 // import Navbar from './Reusable/Navbar';
 // import Banner from './Reusable/Banner';
 
@@ -13,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/login" component={Login} />
         <Route exact path="/signup" component={Signup} />
+        <Route exact path="/classroom/:id" component={Classroom} />
         <Route path="/" component={Page404} />
       </Switch>
     </div>
