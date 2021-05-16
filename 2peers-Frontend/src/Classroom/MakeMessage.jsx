ececa99869b055c0e953e2b29e54988735840ef5
jason <jasonalvarezcoraiza@gmail.com>
2021-05-15 23:30:15 -0400

    styled classroom page and added navbar and footer

--- 2peers-Frontend/src/Classroom/MakeMessage.jsx
+++ 2peers-Frontend/src/Classroom/MakeMessage.jsx
@@ -26,17 +26,17 @@ export default function MakeMessage() {
   }
 
   return (
-    <div>
-      <form onSubmit={sendMessage}>
+    <form onSubmit={sendMessage} className="w-full min-w-sm px-5 py-3">
+      <div className="flex items-center border-b border-green-300 py-2 w-full">
         {/*
           Give the input below a value of the users id
           Conditionally render the name as studentid or
           teacher id according to the props avaiable
         */}
         <input name="id" type="hidden" />
-        <input name="message" placeholder="Type Message here..." type="text" onChange={({ target }) => { setMessage(target.value); }} value={message} />
-        <input type="submit" value="send" />
-      </form>
-    </div>
+        <input name="message" placeholder="Type Message here..." type="text" className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 leading-tight focus:outline-none" onChange={({ target }) => { setMessage(target.value); }} value={message} />
+        <input type="submit" value="Send" className="bg-green-400  hover:bg-green-700 border-green-500 hover:border-green-700 text-lg text-white py-1 px-2 rounded" />
+      </div>
+    </form>
   );
 }
