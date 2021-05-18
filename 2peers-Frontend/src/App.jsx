d75cc3d0c6bb4232414edcd06e2d6928a80074b9
jason <jasonalvarezcoraiza@gmail.com>
2021-05-18 13:51:37 -0400

    made the edit message modal popup and call the associated function

--- 2peers-Frontend/src/App.jsx
+++ 2peers-Frontend/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
       <Switch>
         <Route exact path="/login" component={Login} />
         <Route exact path="/signup" component={Signup} />
-        <Route exact path="/classroom/:id" component={Classroom} />
+        <Route exact path="/classrooms/:id" component={Classroom} />
         <Route exact path="/" component={Home} />
         <Route exact path="/teachers/:id" component={Teacher} />
         <Route exact path="/students/:id" component={Student} />
--- 2peers-Frontend/src/Classroom/Message.jsx
+++ 2peers-Frontend/src/Classroom/Message.jsx
@@ -1,6 +1,7 @@
 import Axios from 'axios';
 import PropTypes from 'prop-types';
 import React, { useEffect, useState } from 'react';
+import EditMessage from './EditMessage';
 
 export default function Message({ text, isStudent, userId }) {
   // const {name, email, message, rating} = props;
@@ -22,23 +23,25 @@ export default function Message({ text, isStudent, userId }) {
   }, [text]);
 
   function options() {
-    console.log(optionsVisible);
     setOptions((prev) => !prev);
   }
 
   return (
-    <div onClick={options} onKeyDown={options} role="button" tabIndex={0} className="message p-3 bg-green-100 ml-2 my-3 rounded overflow-hidden shadow-lg max-w-xs w-44">
-      <div className="rating w-full">
-        ★★★★☆
-      </div>
-      <div className="message-content flex justify-start flex-col pl-3">
-        <div className="message-composer font-bold text-left">
-          { name }
+    <div>
+      <div onClick={options} onKeyDown={options} role="button" tabIndex={0} className="message p-3 bg-green-100 ml-2 my-3 rounded overflow-hidden shadow-lg max-w-xs w-44">
+        <div className="rating w-full">
+          ★★★★☆
         </div>
-        <div className="message-txt text-left">
-          { text }
+        <div className="message-content flex justify-start flex-col pl-3">
+          <div className="message-composer font-bold text-left">
+            { name }
+          </div>
+          <div className="message-txt text-left">
+            { text }
+          </div>
         </div>
       </div>
+      {optionsVisible ? <EditMessage text={text} submission={setOptions} /> : null}
     </div>
   );
 }
