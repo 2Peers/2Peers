ca047f4be68a6046f0668d898fc1fa1d08c07d20
Itzel01 <itzel.areymi.rendon@gmail.com>
2021-05-20 17:28:54 -0400

    can login depending on role

--- 2peers-Frontend/src/App.jsx
+++ 2peers-Frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import React, { useContext } from 'react';
-import { Redirect, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Login from './data/Login';
 import Signup from './data/Signup';
 import Page404 from './data/Page404';
 import Classroom from './Classroom/Classroom';
 import Home from './Home/Home';
 import Teacher from './Teacher/Teacher';
-// import Student from './Student/Student';
+import Student from './Student/Student';
 // import Message from './Classroom/Message';
 // import Navbar from './Reusable/Navbar';
 // import Banner from './Reusable/Banner';
@@ -22,9 +22,9 @@ function App() {
         <Route exact path="/signup" component={Signup} />
         <Route exact path="/classrooms/:id" component={Classroom} />
         <Route exact path="/" component={Home} />
-        <Route exact path="/teachers/:id">
-          { data.valid && data.role === 'student' ? <Redirect to="/students/:id" /> : <Teacher />}
-        </Route>
+        <Route exact path="/teachers/:id" component={Teacher} />
+        {/* { data.valid && data.role === 'student' ?<Redirect to="/students/:id"/>:<Teacher />} */}
+        <Route exact path="/student/:id" component={Student} />
         <Route path="/" component={Page404} />
       </Switch>
     </div>
