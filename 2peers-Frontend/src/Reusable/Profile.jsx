41ff21e8f1c444b5cf5585a2a26761c48e581a79
jason <jasonalvarezcoraiza@gmail.com>
2021-05-19 23:47:42 -0400

    working on edit student modal

--- 2peers-Frontend/src/Reusable/Profile.jsx
+++ 2peers-Frontend/src/Reusable/Profile.jsx
@@ -2,11 +2,13 @@ import Axios from 'axios';
 import PropTypes from 'prop-types';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import EditStudent from '../Student/EditStudent';
 
 export default function Profile({ isStudent }) {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [pic, setPic] = useState('');
+  const [edit, setEdit] = useState(false);
   const { id } = useParams();
 
   useEffect(() => {
@@ -28,7 +30,7 @@ export default function Profile({ isStudent }) {
   }, []);
 
   return (
-    <div className="profile-container w-11/12 my-2 rounded shadow-lg flex justify-start">
+    <div className="profile-container w-11/12 my-8 rounded shadow-lg flex justify-start">
       <div className="prof-img h-40 w-1/4">
         <img src={pic} alt="Profile" />
       </div>
@@ -42,10 +44,11 @@ export default function Profile({ isStudent }) {
           </p>
         </div>
         <div className="user-btns py-5 flex w-3/12 justify-around">
-          <button type="button" className="mx-3 bg-transparent hover:bg-green-400 text-green-700 font-semibold hover:text-white py-2 px-4 border border-green-400 hover:border-transparent rounded">Edit</button>
+          <button type="button" onClick={() => { setEdit((prev) => !prev); }} className="mx-3 bg-transparent hover:bg-green-400 text-green-700 font-semibold hover:text-white py-2 px-4 border border-green-400 hover:border-transparent rounded">Edit</button>
           <button type="button" className="mx-3 bg-transparent hover:bg-green-400 text-green-700 font-semibold hover:text-white py-2 px-4 border border-green-400 hover:border-transparent rounded">Delete</button>
         </div>
       </div>
+      { edit ? <EditStudent /> : null}
     </div>
   );
 }
